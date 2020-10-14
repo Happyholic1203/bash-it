80090d0155070402237dfafa2fcf97510bfe7ed7
Yu-Cheng (Henry) Huang <Happyholic1203@gmail.com>
2020-10-14 20:22:28 +0800

    Add plugin: short-git

---
+++ plugins/available/short-git.plugin.bash
@@ -0,0 +1,20 @@
+cite about-plugin
+about 'Shorter `git` to `g` while keeping all the autocompletion working'
+
+g() {
+    if [ $# -gt 0 ]; then
+        cmd="git"
+        for arg in "$@"; do
+            cmd+=" \"${arg//\"/\\\"}\""
+        done
+        eval $cmd
+    elif _command_exists gitstatus; then
+        gitstatus
+    else
+        git status
+    fi
+}
+
+# Complete g like git
+_completion_loader git 2>/dev/null
+eval $(complete -p git | sed 's/ git$/ g/g')
