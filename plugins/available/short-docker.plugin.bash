9cd959a5d446e0ea7f0f6e7ef7e8e97a8f7905e2
Yu-Cheng (Henry) Huang <Happyholic1203@gmail.com>
2020-10-14 20:23:51 +0800

    Add plugin: short-docker

---
+++ plugins/available/short-docker.plugin.bash
@@ -0,0 +1,18 @@
+cite about-plugin
+about 'Shorter `docker` to `d` while keeping all the autocompletion working'
+
+d() {
+    if [ $# -gt 0 ]; then
+        cmd="docker"
+        for arg in "$@"; do
+            cmd+=" \"${arg//\"/\\\"}\""
+        done
+        eval $cmd
+    else
+        docker ps
+    fi
+}
+
+# Complete d like docker
+_completion_loader docker 2>/dev/null
+eval $(complete -p docker | sed 's/ docker$/ d/g')
